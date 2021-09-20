c2fe6ee949dad96265b046886e96933c1d053755
portfolio-y0711 <portfolio-y0711@gmail.com>
2021-09-21 07:36:13 +0900

    fix: modify seeder  & build scripts to make application executible with command line java

--- src/main/java/com/portfolioy0711/api/data/seeder/DataSeeer.java
+++ src/main/java/com/portfolioy0711/api/data/seeder/DataSeeer.java
@@ -7,29 +7,39 @@ import com.portfolioy0711.api.data.models.user.UserCmdRepository;
 import com.portfolioy0711.api.data.entities.Place;
 import com.portfolioy0711.api.data.entities.User;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
-public class DataSeeer implements CommandLineRunner {
+public class DataSeeer implements CommandLineRunner, ApplicationRunner {
     private final Environment env;
     private final UserCmdRepository userRepository;
     private final PlaceCmdRepository placeRepository;
 
     public void seedUsers() throws IOException {
-        final File resource = new ClassPathResource("/seeds/users.json").getFile();
-        final String json = new String(Files.readAllBytes(resource.toPath()));
+//        final File resource = new ClassPathResource("/seeds/users.json").getFile();
+        final ClassPathResource classPathResource = new ClassPathResource("/seeds/users.json");
+        final InputStreamReader isr = new InputStreamReader(classPathResource.getInputStream());
+        final Stream<String> streamOfString = new BufferedReader(isr).lines();
+        final String json = streamOfString.collect(Collectors.joining());
+//        final String json = new String(Files.readAllBytes(resource.toPath()));
 
         final ObjectMapper objectMapper = new ObjectMapper();
         final List<User> users = objectMapper.readValue(json, new TypeReference<List<User>>(){});
@@ -38,8 +48,13 @@ public class DataSeeer implements CommandLineRunner {
     }
 
     public void seedPlaces() throws IOException {
-        final File resource = new ClassPathResource("/seeds/places.json").getFile();
-        final String json = new String(Files.readAllBytes(resource.toPath()));
+//        final File resource = new ClassPathResource("/seeds/places.json").getFile();
+        final ClassPathResource classPathResource = new ClassPathResource("/seeds/places.json");
+        final InputStreamReader isr = new InputStreamReader(classPathResource.getInputStream());
+        final Stream<String> streamOfString = new BufferedReader(isr).lines();
+        final String json = streamOfString.collect(Collectors.joining());
+
+//        final String json = new String(Files.readAllBytes(resource.toPath()));
 
         final ObjectMapper objectMapper = new ObjectMapper();
         final List<Place> places = objectMapper.readValue(json, new TypeReference<List<Place>>(){});
@@ -49,9 +64,18 @@ public class DataSeeer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(Arrays.toString(env.getActiveProfiles()));
+        final boolean isProduction = Arrays.stream(env.getActiveProfiles()).collect(Collectors.toList()).contains("production");
+        if (isProduction) {
+            seedUsers();
+            seedPlaces();
+        }
+    }
 
-        final boolean isProduction = Arrays.stream(env.getActiveProfiles()).collect(Collectors.toList()).contains("production") || Arrays.stream(args).collect(Collectors.toList()).contains("production");
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+
+        final boolean isProduction = args.getNonOptionArgs().contains("production");
         if (isProduction) {
             seedUsers();
             seedPlaces();
